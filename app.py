bc394bb7e70b4024710a7ef612224ef2bd4f52e6
C_Jyo <minalikong@gmail.com>
2022-11-16 10:09:52 +0900

    쿠키 확인하여 아이디, 닉네임 전달

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
-from api import login # api 폴더의 login.py 파일을 가져옵니다.
+from flask import Flask, render_template, jsonify
+from api import login_join # api 폴더의 login.py 파일을 가져옵니다.
 
 app = Flask(__name__)
 
-app.register_blueprint(login.login_api) # login.py 파일의 login_api 블루프린트를 연결해줍니다.
+app.register_blueprint(login_join.login_api) # login.py 파일의 login_api 블루프린트를 연결해줍니다.
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_info = login_join.check_login()
+    print(user_info)
+
+    if(user_info['result'] == 'success'):
+        return render_template('index.html', id = user_info['id'], nick = user_info['nick'])
+    else:
+        return render_template('index.html', msg = user_info['msg'])
 
 
 @app.route('/login')
