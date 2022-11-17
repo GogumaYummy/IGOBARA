269afe84a65a13608de49d1afdc9bf70c2374a5a
GogumaYummy <sora1211x@gmail.com>
2022-11-17 10:59:07 +0900

    게시글 상세보기

--- api/articles.py
+++ api/articles.py
@@ -1,22 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import request, jsonify, Blueprint
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import certifi
+# 여기서 사용한 패키지는 app.py에서 다시 불러올 필요가 없습니다.
 
 ca = certifi.where()
+
 client = MongoClient('mongodb+srv://igobara:221114@cluster0.pmylkqu.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
-test_api = Blueprint("test_api", __name__, url_prefix="/test")
+post_api = Blueprint("post", __name__, url_prefix="/api")
 
-@test_api.route('/dummyarticles', methods=['POST'])
-def create_dummy_articles():
-    for i in range(100):
-        doc = {
-            'postedBy': 'ㅇ',
-            'title': str(i) + '번째 게시글',
-            'content': '내용',
-            'image': 'https://cdn.travie.com/news/photo/first/201710/img_19975_1.jpg'
-        }
-        db.articles.insert_one(doc)
 
-    return jsonify({'msg': 'success'})
+@post_api.route('/article/<id>')
+def post(id):
+    _id = ObjectId(id)
+    result = db.articles.find_one({ '_id' : _id })
+    result['_id'] = str(result['_id'])
+
+    return jsonify({'result': result })
+
+
+@post_api.route('/delete', methods=["POST"])
+def delete():
+    id = request.form['_id']
+    result = db.post.delete_one({"_id" : id})
+    return jsonify({'msg': '삭제완료'})
--- app.py
+++ app.py
@@ -1,38 +1,59 @@
 from flask import Flask, render_template
-from api import login_join, test # api 폴더의 파일을 가져옵니다.
-from api import post
+from api import articles, login_join, test # api 폴더의 파일을 가져옵니다.
 
 app = Flask(__name__)
 
 app.register_blueprint(login_join.login_api) # login.py 파일의 login_api 블루프린트를 연결해줍니다.
 app.register_blueprint(test.test_api)
-app.register_blueprint(post.post_api)
+app.register_blueprint(articles.post_api)
 
-def render(template):
+
+@app.route('/')
+def index():
     user_info = login_join.check_login()
     result = user_info['result']
 
     if(user_info['result'] == 'success'):
-        id = user_info['id']
+        userId = user_info['id']
         nick = user_info['nick']
 
-        return render_template(template, result = result, id = id, nick = nick)
+        return render_template('index.html', result = result, userId = userId, nick = nick)
     else:
         msg = user_info['msg']
 
-        return render_template(template, result = result, msg = msg)
-
-@app.route('/')
-def index():
-    return render('index.html')
+        return render_template('index.html', result = result, msg = msg)
 
 @app.route('/article/<id>')
-def post(id):
-    return render_template('post.html',id = id)
+def article(id):
+    user_info = login_join.check_login()
+    result = user_info['result']
+    articleId = id
+    print(articleId)
+
+    if(user_info['result'] == 'success'):
+        userId = user_info['id']
+        nick = user_info['nick']
+
+        return render_template('post.html', result = result, userI= userId, nick = nick, articleId = articleId)
+    else:
+        msg = user_info['msg']
+
+        return render_template('post.html', result = result, msg = msg, articleId = articleId)
 
 @app.route('/login')
 def login():
-    return render('login.html')
+    user_info = login_join.check_login()
+    result = user_info['result']
+
+    if(user_info['result'] == 'success'):
+        userId = user_info['id']
+        nick = user_info['nick']
+
+        return render_template('login.html', result = result, userId = userId, nick = nick)
+    else:
+        msg = user_info['msg']
+
+        return render_template('login.html', result = result, msg = msg)
 
 
 
